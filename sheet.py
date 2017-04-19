d9132bb735b5431f70f2c12aa9e7e6c22642414b
inuyasha2012 <chinachrist@163.com>
2017-04-19 18:10:17 +0800

    init

--- sheet.py
+++ sheet.py
@@ -9,9 +9,16 @@ from utils import detect_cnt_again, get_init_process_img, get_bright_process_img
 
 
 def get_answer_from_sheet(base_img):
+
+    # cv2.imshow('temp', base_img)
+    # cv2.waitKey(0)
+
     # 灰度化然后进行边缘检测、二值化等等一系列处理
     img = cv2.cvtColor(base_img, cv2.COLOR_BGR2GRAY)
     img = get_init_process_img(img)
+
+    # cv2.imshow('temp', img)
+    # cv2.waitKey(0)
 
     # 获取最大面积轮廓并和图片大小作比较，看轮廓周长大小判断是否是答题卡的轮廓
     cnt = get_max_area_cnt(img)
@@ -19,6 +26,10 @@ def get_answer_from_sheet(base_img):
     base_img_perimeter = (base_img.shape[0] + base_img.shape[1]) * 2
     if not cnt_perimeter > CNT_PERIMETER_THRESHOLD * base_img_perimeter:
         raise ContourPerimeterSizeError
+
+    # cv2.drawContours(base_img, [cnt], 0, (0, 255, 0), 1)
+    # cv2.imshow('temp', base_img)
+    # cv2.waitKey(0)
 
     # 计算多边形的顶点，并看是否是四个顶点
     poly_node_list = cv2.approxPolyDP(cnt, cv2.arcLength(cnt, True) * 0.1, True)
@@ -28,8 +39,14 @@ def get_answer_from_sheet(base_img):
     # 根据计算的多边形顶点继续处理图片，主要是是纠偏
     processed_img = detect_cnt_again(poly_node_list, base_img)
 
+    # cv2.imshow('temp', processed_img)
+    # cv2.waitKey(0)
+
     # 调整图片的亮度
     processed_img = get_bright_process_img(processed_img)
+
+    # cv2.imshow('temp', processed_img)
+    # cv2.waitKey(0)
 
     # 通过二值化和膨胀腐蚀获得填涂区域
     ret, ans_img = cv2.threshold(processed_img, ANS_IMG_THRESHOLD[0], ANS_IMG_THRESHOLD[1], cv2.THRESH_BINARY_INV)
@@ -37,11 +54,17 @@ def get_answer_from_sheet(base_img):
     ans_img = cv2.erode(ans_img, ANS_IMG_KERNEL, iterations=ANS_IMG_ERODE_ITERATIONS)
     ret, ans_img = cv2.threshold(ans_img, ANS_IMG_THRESHOLD[0], ANS_IMG_THRESHOLD[1], cv2.THRESH_BINARY_INV)
 
+    # cv2.imshow('temp', ans_img)
+    # cv2.waitKey(0)
+
     # 通过二值化和膨胀腐蚀获得选项框区域
     ret, choice_img = cv2.threshold(processed_img, CHOICE_IMG_THRESHOLD[0], CHOICE_IMG_THRESHOLD[1],
                                     cv2.THRESH_BINARY_INV)
     choice_img = cv2.dilate(choice_img, CHOICE_IMG_KERNEL, iterations=CHOICE_IMG_DILATE_ITERATIONS)
     choice_img = cv2.erode(choice_img, CHOICE_IMG_KERNEL, iterations=CHOICE_IMG_ERODE_ITERATIONS)
+
+    # cv2.imshow('temp', choice_img)
+    # cv2.waitKey(0)
 
     # 查找选项框以及前面题号的轮廓
     cnts, h = cv2.findContours(choice_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -51,10 +74,10 @@ def get_answer_from_sheet(base_img):
     for i, c in enumerate(cnts):
         # 如果面积小于某值，则认为这个轮廓是选项框或题号
         if cv2.contourArea(c) < CHOICE_MAX_AREA:
-            cv2.drawContours(temp_ans_img, cnts, i, (0, 255, 0), 1)
+            cv2.drawContours(temp_ans_img, cnts, i, (0, 0, 0), 1)
             question_cnts.append(c)
 
-    # cv2.imshow('fuck', base_img)
+    # cv2.imshow('temp', temp_ans_img)
     # cv2.waitKey(0)
 
     # 如果轮廓小于特定值，重新扫描
